45f9c331095f90232e0a8fee2d6b5d4d63f8273b
Edvards Dergacevs <edvards.dergacevs@gmail.com>
2021-04-13 14:46:27 +0300

    Customized using templating engine

--- views/subscription.php
+++ views/subscription.php
@@ -1,17 +1,22 @@
+<?php
+use app\core\blade\form\Form;
+
+$form = new Form();
+?>
+
 <h1>Subscribe to newsletters</h1>
 <h2>Subscribe to our newsletter and get 10% discount on pineapple glasses.</h2>
-<form id="f" method="POST"  action="">
-    <span class="input-line"></span>
-    <input id="email" name="email" type="text" placeholder="Type your email address here…">
 
+<?php $form = Form::begin('', 'post', 'novalidate') ?>
+    <span class="input-line"></span>
+    <?php echo $form->input($sub, 'email', [])->email()->required() ?>
     <div class="tos">
         <div class="container">
-            <input id="tos" type="checkbox" name="tos">
             <span class="checkbox">
                 <img src="img/ic_checkmark.svg">
             </span>
+            <?php echo $form->checkbox($sub, 'tos')->required() ?>
         </div>
-        <p>I agree to <a href="#" class="link">terms of service</a></p>
     </div>
     <button id="arrow" type='submit'></button>
-</form>      
+<?php $form = Form::end() ?>      
