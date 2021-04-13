d42851551aea2d5802fa316fc0cfe7046a98d44a
Edvards Dergacevs <edvards.dergacevs@gmail.com>
2021-04-13 14:23:11 +0300

    Configuration file for database and application

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_DSN', 'mysql:dbname=pineapple;host=localhost;port=3308');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+
+define('APP_DIR', __DIR__);
+define('APP_URL', 'http://localhost');
