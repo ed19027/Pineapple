3b2bca0ef4b3e8c38ba065fd627cf8bf2df5d663
Edvards Dergacevs <edvards.dergacevs@gmail.com>
2021-04-21 02:30:03 +0300

    Syntax adjustment for redesigned templating engine

--- views/subscription.php
+++ views/subscription.php
@@ -4,19 +4,18 @@ use app\core\blade\form\Form;
 $form = new Form();
 ?>
 
-<h1>Subscribe to newsletters</h1>
-<h2>Subscribe to our newsletter and get 10% discount on pineapple glasses.</h2>
-
+<div class="subscribe">
+  <h1>Subscribe to newsletter</h1>
+  <h2>Subscribe to our newsletter and get 10% discount on pineapple glasses.</h2>
+</div>
 <?php $form = Form::begin('', 'post', 'novalidate') ?>
-    <span class="input-line"></span>
-    <?php echo $form->input($sub, 'email', [])->email()->required() ?>
-    <div class="tos">
-        <div class="container">
-            <span class="checkbox">
-                <img src="img/ic_checkmark.svg">
-            </span>
-            <?php echo $form->checkbox($sub, 'tos')->required() ?>
-        </div>
-    </div>
-    <button id="arrow" type='submit'></button>
+  <?php
+  echo $form->input(
+    $sub,
+    'email',
+    ['pattern' => '^[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)(?<!(?:.co))$']
+  )->email()->placeholder('Type your email address here...')->required();
+  
+  echo $form->checkbox($sub, 'tos', 'agreed')->required();
+  ?>
 <?php $form = Form::end() ?>      
