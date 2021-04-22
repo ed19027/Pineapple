37a363f752162a50dfae87a08a2a42953524a268
Edvards Dergacevs <edvards.dergacevs@gmail.com>
2021-04-22 11:46:01 +0300

    Description added

--- core/blade/form/Form.php
+++ core/blade/form/Form.php
@@ -5,6 +5,11 @@ use app\core\Model;
 
 class Form
 {
+    /**
+     * Print begining of the form field.
+     *
+     * @return app\core\blade\form\Form
+     */
     public static function begin($action, $method, $option = '')
     {
         echo sprintf(
@@ -16,16 +21,29 @@ class Form
         return new Form();
     }
 
+    /**
+     * Print end of the form field.
+     */
     public static function end()
     {
         echo '</form>';
     }
 
+    /**
+     * initializing new input field.
+     *
+     * @return app\core\blade\form\Input
+     */
     public function input(Model $model, string $attribute, $rules = [])
     {
         return new Input($model, $attribute, $rules);
     }
 
+    /**
+     * initializing new checkbox field.
+     *
+     * @return app\core\blade\form\Checkbox
+     */
     public function checkbox(Model $model, string $attribute, string $value)
     {
         return new Checkbox($model, $attribute, $value);
